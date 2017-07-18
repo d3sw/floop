b9fa65629e2e0f0c1f2064b3899f951a0a558f19
euforia <euforia>
2017-07-18 16:57:24 -0700

    Added comments

--- handlers/echo.go
+++ handlers/echo.go
@@ -13,7 +13,9 @@ func (lc *EchoHandler) Init(*types.HandlerConfig) error {
 	return nil
 }
 
-// Handle echos back input data before process starts
+// Handle echos back input data before process starts.  The config is the normalized
+// config built using data from the child process.  This may be different from the one
+// used in Init
 func (lc *EchoHandler) Handle(event *types.Event, conf *types.HandlerConfig) (map[string]interface{}, error) {
 	fmt.Printf("[Echo] phase=%s %+v\n", event.Type, event.Data)
 	return nil, nil
